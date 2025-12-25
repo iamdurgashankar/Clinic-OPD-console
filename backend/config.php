26702912ae05fb088d292000c0b7719061afa4d2
Durga Shankar Das Mangaraj <durgashankardasmangaraj@Durgas-MacBook-Air.local>
2025-12-25 23:47:52 +0530

    Update database credentials for production

--- backend/config.php
+++ backend/config.php
@@ -1,7 +1,7 @@
 <?php
 // Database Configuration
 define('DB_HOST', 'localhost');
-define('DB_NAME', 'u180145459_ThM3m');
-define('DB_USER', 'u180145459_TZhM2pS');
-define('DB_PASS', 'Rajopd@12');
+define('DB_NAME', 'u180145459_rajopdconsole');
+define('DB_USER', 'u180145459_opdconsole');
+define('DB_PASS', 'Raj@dsdm0012');
 ?>
